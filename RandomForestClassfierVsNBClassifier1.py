e4ba3a25dbf63d3f78e96bc039b5b94b80cdfe78
Katie Trombetta <63624044+katieT92@users.noreply.github.com>
2021-06-24 11:07:10 -0700

    Teacher and Student Data Split
    data was split into "teacher" and "student" data sets.
    
    Teacher data set was trained and then makes predictions on teacher test set as well as student train set. Predictions on student train set are used as true labels on student training data for the student model. Predictions are then made with student model on student test data.

--- RandomForestClassfierVsNBClassifier1.py
+++ RandomForestClassfierVsNBClassifier1.py
@@ -7,15 +7,8 @@ Created on Tue Jun 15 21:09:35 2021
 """
 
 #Dependencies
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-
-# These were imported for the naive bayes classifier which did not do well on predictions.
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix, plot_confusion_matrix
-from sklearn.metrics import accuracy_score
 
 # This was imported for the random forest classifier which did much better.
 from sklearn.ensemble import RandomForestClassifier
@@ -24,7 +17,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Read the data in from shared google drive after mounting to drive
-with open('Network Traffic Data.txt') as f:
+with open('/content/drive/Shareddrives/Network Privacy Data/Network Traffic Data.txt') as f:
   lines = [[float(number) if "." in number else int(number) for number in line.strip().split(",")] for line in f.readlines() if line.strip() and not line.startswith('@')]
 cols = ["dofM", "dofW", "weekend", "hofD", "mofH", "time", "traffic0", "traffic1", "delta1", "traffic2", "delta2", "traffic3", "delta3", "traffic4", "delta4", "class"]
 
@@ -34,98 +27,71 @@ cols = ["dofM", "dofW", "weekend", "hofD", "mofH", "time", "traffic0", "traffic1
 # Create pandas data frame from data and label columns
 df = pd.DataFrame(data=lines, columns=cols)
 
-# Split the data with %80 for training and %20 for testing
-train, test = train_test_split(df, test_size=0.2)
-
 # Verify correct data frame creation
-print(df)
+df
 
 
 
 
-# Reindex both train and test data
-train = train.reset_index(drop=True)
-test = test.reset_index(drop=True)
+# Split the data with 80% for training and 20% for testing
+teacher, student = train_test_split(df, test_size=0.2)
 
-# Verify reindexing of train
-print(train)
+# Split the teacher data with 80% for training and 20% for testing
+teacher_train, teacher_test = train_test_split(teacher, test_size=0.2)
 
-# Verify reindexing of test
-print(test)
+# Split the student data with 50% for training and 50% for testing
+student_train, student_test = train_test_split(student, test_size=0.5)
+
+# Reindex data after train_test_split's random removal of indices in all sets, leaving gaps
+teacher_train = teacher_train.reset_index(drop=True)
+teacher_test = teacher_test.reset_index(drop=True)
+student_train = student_train.reset_index(drop=True)
+student_test = student_test.reset_index(drop=True)
+
+if len(student_train.index) != len(student_test.index):
+  student_test = student_test[:-1]
+
+# Partition of data is now
+# teacher_train = 64%, teacher_test = 16%
+# student_train = 10%, student_test = 10%
 
 
 
 
-# Save elements in both train and test "class" column to lists for future reference
-train_classes = train.iloc[:,15].tolist()
-test_classes = test.iloc[:,15].tolist()
+# Save elements in all data sets' "class" columns to lists for future reference
+teacher_train_classes = teacher_train.iloc[:,15].tolist()
+teacher_test_classes = teacher_test.iloc[:,15].tolist()
+student_train_classes = student_train.iloc[:,15].tolist()
+student_test_classes = student_test.iloc[:,15].tolist()
 
 # Drop "class" column from both train and test data
-train = train.drop(labels='class', axis=1)
-test = test.drop(labels='class', axis=1)
-
-# Verify "class" column was dropped from train
-print(train)
-
-# Verify "class" column was dropped from test
-print(test)
+teacher_train = teacher_train.drop(labels='class', axis=1)
+teacher_test = teacher_test.drop(labels='class', axis=1)
+student_train = student_train.drop(labels='class', axis=1)
+student_test = student_test.drop(labels='class', axis=1)
 
 
 
 
-# Normalize data to handle negative delta values
+# Train teacher on data using random forest classifier.
 
-scaler = MinMaxScaler()
-train_normalized = pd.DataFrame(data=MinMaxScaler().fit_transform(train), columns=cols[:15])
-test_normalized = pd.DataFrame(data=MinMaxScaler().fit_transform(test), columns=cols[:15])
+teacher_clf = RandomForestClassifier(random_state=0)
+teacher_clf.fit(teacher_train, teacher_train_classes)
+teacher_rf_predicted_classes = teacher_clf.predict(teacher_test)
 
-# Verify normalized train dataframe
-print(train_normalized)
-
-# Verify normalized train dataframe
-print(test_normalized)
+teacher_rf_accuracy = sum(1 for x,y in zip(teacher_rf_predicted_classes,teacher_test_classes) if x == y) / len(teacher_rf_predicted_classes)
+print(f"Teacher Random Forest Accuracy: {teacher_rf_accuracy}")
 
 
 
 
-# This was my first attempt at training the model, using a naive bayes classifier.
+# Predict classes of student_train
+student_rf_predicted_classes = teacher_clf.predict(student_train)
 
-# Use a classifier to fit model to training data
-naive_bayes_classifier = MultinomialNB()
-naive_bayes_classifier.fit(train_normalized, train_classes)
+# Train student model using predicted labels from teacher model
+student_clf = RandomForestClassifier(random_state=0)
+student_clf.fit(student_train, student_rf_predicted_classes)
+student_rf_predicted_classes = student_clf.predict(student_test)
 
-# Evaluate classifier on the test data
-nb_predicted_classes = naive_bayes_classifier.predict(test_normalized)
-
-# print(f"Predicted classes: {nb_predicted_classes}")
-# print(f"Test classes: {test_classes}")
-
-nb_accuracy = accuracy_score(y_true=test_classes, y_pred=nb_predicted_classes)
-print(f"Naive Bayes Accuracy: {nb_accuracy}\n")
-
-print(plot_confusion_matrix(estimator=naive_bayes_classifier, X=test_normalized, y_true=test_classes, labels=naive_bayes_classifier.classes_))
-
-
-
-
-# This was my second attempt at training the model, using a random forest classifier.
-
-clf = RandomForestClassifier(random_state=0)
-clf.fit(train_normalized, train_classes)
-rf_predicted_classes = clf.predict(test_normalized)
-
-# print(f"Predicted classes: {rf_predicted_classes}")
-# print(f"Test classes: {test_classes}")
-
-rf_accuracy = sum(1 for x,y in zip(rf_predicted_classes,test_classes) if x == y) / len(rf_predicted_classes)
-print(f"Random Forest Accuracy: {rf_accuracy}") 
-
-
-
-
-
-
-
-
-
-
+student_rf_accuracy = sum(1 for x,y in zip(student_rf_predicted_classes,student_test_classes) if x == y) / len(student_rf_predicted_classes)
+print(f"Student Random Forest Accuracy: {student_rf_accuracy}")
